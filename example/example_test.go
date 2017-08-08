d9af32b8acbe33cfcbdb9d4f54f38c665a725b9c
Ben Meier <benmeier@fastmail.com>
2017-08-09 00:07:42 +0200

    added output to rendered examples, fixed link anchors. Closes #1

--- example/example_test.go
+++ example/example_test.go
@@ -9,13 +9,16 @@ import (
 // Example_of_stuff shows an example of various random things.
 func Example_of_stuff() {
 	fmt.Println(example.T0, example.T1, example.T2)
+	// Output: 0 1 2
 }
 
 func ExampleTopLevelFunk() {
 	example.TopLevelFunk(1, "hi")
+	// Output: 1 ["hi"]
 }
 
 func ExampleSomeType_PointerReceiver() {
 	t := example.SomeType{}
 	t.PointerReceiver(1)
+	// Output: 1
 }
